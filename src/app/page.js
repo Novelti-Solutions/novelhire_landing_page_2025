94c43a74037807b8967eebfbb23216ec86133a74
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2025-01-03 13:32:12 +0530

    Remove duplicate FeaturesSection component from Home layout

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <HomeLayout>
       <FeaturesSection />
-      <FeaturesSection />
       <HowItWorks />
       <CallToAction/>
       <FAQSection />
