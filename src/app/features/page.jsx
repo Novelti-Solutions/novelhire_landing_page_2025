aa63897a51edfddb466798290b3617269758e8a2
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2025-01-03 13:27:35 +0530

    Update FeaturesPage component layout and re-enable FeaturesSection in Home

--- src/app/features/page.jsx
+++ src/app/features/page.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function FeaturesPage() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div>
+        <p>Assess Skills with Precision</p>
+        <h1>
+          Smart, Customizable <span>Assessmentss </span>
+        </h1>
+      </div>
+      <div></div>
+    </div>
+  );
 }
